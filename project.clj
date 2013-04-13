2b28d7866adc5d402325d2e937875fbc0274a0fb
shakedown <a1979shakedown@gmail.com>
2013-04-13 14:32:57 -0700

    Part 2: Added game loop, UIs, and input

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject caves-of-clojure "0.1.0-SNAPSHOT"
   :description "Following Steve Losh's blog series"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/shakedown/caves"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
