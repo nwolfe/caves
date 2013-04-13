734316770bce044e5af72172abc593c60954122d
shakedown <a1979shakedown@gmail.com>
2013-04-13 03:34:26 -0700

    Simplify main

--- src/caves/core.clj
+++ src/caves/core.clj
@@ -2,17 +2,11 @@
   (:gen-class)
   (:require [lanterna.screen :as s]))
 
-(defn main
-  [screen-type]
-  (let [screen (s/get-screen screen-type)]
+(defn -main
+  [& args]
+  (let [screen (s/get-screen :auto)]
     (s/in-screen screen
                  (s/put-string screen 0 0 "Welcome to Caves of Clojure!")
                  (s/put-string screen 0 1 "Press any key to exit...")
                  (s/redraw screen)
                  (s/get-key-blocking screen))))
-
-(defn -main
-  [& args]
-  (let [args (set args)
-        screen-type :auto]
-    (main screen-type)))
